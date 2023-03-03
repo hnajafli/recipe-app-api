142e5a42477409b0bbaac361fd9f539b8b75a911
hnajafli <hnajafli@naa.edu.az>
2023-03-03 16:01:21 +0400

    Docker-compose updated

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+Calculator functions
+"""
+
+
+def add(a, b):
+    """Sum of two numbers and return result."""
+    return a + b
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Tests the calc method."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
