b47ce50c4bcfcfac70fa3e457119746eaf6e686e
hnajafli <hnajafli@naa.edu.az>
2023-03-06 23:37:48 +0400

    test_commands.py file created

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+# Importing the BaseCommand class from the django.core.management.base module.
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, **argsm ** options):
+        pass
